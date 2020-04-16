59f76551f2422a1fe67c0681fcd4e73a3e4e08e0
RikardLof <rikardlof@gmail.com>
2020-04-16 15:51:58 +0200

    Påbörjad kod i Game samt tomma func i Board

--- www/Board.js
+++ www/Board.js
@@ -1,4 +1,20 @@
-class Board { }
+module.exports = class Board {
+
+  constructor(game) { }
+
+  async makeMove(column) { }
+
+  winCheck() { }
+
+  markWin(combo) { }
+
+  addEventListener() { }
+
+  removeEventListener() { }
+
+  render() { }
+
+}
 
 // make it possible to test on backend
 if (typeof global !== 'undefined') { global.Board = Board };
--- www/Game.js
+++ www/Game.js
@@ -1,4 +1,40 @@
-class Game { }
+module.exports = class Game {
+
+  constructor() {
+    this.board = {};
+    this.listener = addEventListener();
+    this.start();
+  }
+
+  start() {
+    this.board = new Board(this);
+  }
+
+  tellTurn(player) {
+    if (player !== 1 || player !== 2) throw console.error('player must be 1 or 2');
+    if (player === 1) { $('.message').innerHTML = 'Röds tur...'; } else { $$('.message').innerHTML = 'Guls tur...'; }
+  }
+
+  over(won) {
+    if (won !== 1 || won !== 2 || won !== 'draw') throw console.error('won must be “draw”, 1 or 2');
+    let message = 'Det blev oavgjort!';
+    switch (won) {
+      case 1: message = 'Röd vann!'; break;
+      case 2: message = 'Gul vann!'; break;
+    }
+    $('.message').innerHTML = message + ' <button type="button" id="playAgainButton">Spela igen</button>';
+  }
+
+  addEventListener() {
+    if (document.querySelector('[id="playAgainButton"]')) {
+      document.addEventListener("click", function (e) { this.start(); });
+    }
+    else {
+      throw console.error('Could not add eventlistener! Play again button element was not found');
+    }
+  }
+
+}
 
 // make it possible to test on backend
 if (typeof global !== 'undefined') { global.Game = Game };

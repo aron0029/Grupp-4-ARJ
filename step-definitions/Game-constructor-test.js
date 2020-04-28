d263f7684d326968e643ed9ba21248e51d573da4
RikardLof <rikardlof@gmail.com>
2020-04-28 18:41:05 +0200

    Ändringar makeMove feature & test m.m

--- step-definitions/Game-constructor-test.js
+++ step-definitions/Game-constructor-test.js
@@ -16,24 +16,44 @@ module.exports = function () {
     addEventListener() { addEventListenerCalled = true; }
   }
 
+
+  /* ------------------------------------------------------------------------------------ */
+  /* ---------- Scenario: Starting the game by creating a new instance of game ---------- */
+  /* ------------------------------------------------------------------------------------ */
+
   this.Given(/^that the game is started by creating a new instance of Game$/, function () {
+
     fakeGame = new FakeGame();
-    expect(startCalled).to.equal(true);
+
+    expect(startCalled).to.be.true;
+
   });
+
 
   this.Then(/^game property board should be set to a new instance of Board by calling game start method$/, function () {
+
     realGame = new Game();
+
     expect(realGame.board).to.be.instanceof(Board);
+
   });
+
 
   this.Then(/^game addEventListener method should be called$/, function () {
-    expect(addEventListenerCalled).to.equal(true);
+
+    expect(addEventListenerCalled).to.be.true;
+
   });
 
+
   this.Then(/^game tellTurn method should be called by board with board currentPlayer value (\d+) as argument$/, function (value) {
-    fakeGame.board = new Board(fakeGame); // Creating manually since Game start method override in FakeGame
+
+    // Creating manually since Game start method override in FakeGame
+    fakeGame.board = new Board(fakeGame);
+
     expect(currentPlayer).to.equal(fakeGame.board.currentPlayer);
     expect(currentPlayer).to.equal(+value);
+
   });
 
 }

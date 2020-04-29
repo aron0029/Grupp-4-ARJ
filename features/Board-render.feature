449c95d804d0918008fc5bf4e7917608a75424b8
RikardLof <rikardlof@gmail.com>
2020-04-29 11:12:27 +0200

    Justering Board-render feat & test

--- features/Board-render.feature
+++ features/Board-render.feature
@@ -8,18 +8,18 @@ Feature: 4-in-a-row game graphical user interface
     Then 42 html div elements should be added as children of that html div element each with a html div element child of their own
 
   Scenario: Game should update GUI when valid move is made by red player 1
-    Given that html div element with class .board exists when board render method is called when player 1 makes a move
-    And that board render method was called after move was made
-    When board currentPlayer property value was 1
+    Given that html div element with class .board exists when board render method is called when red player 1 makes a move
+    And that board render method was called after move was made by red player
+    When board currentPlayer property value was 1 on move by red player
     Then class .red should be added to one of the 42 html div elements corresponding to last player move
-    And all previous player moves should remain visible and correspond to values in property board matrix array
+    And all previous moves should remain visible including red player 1's and correspond to values in property board matrix array
 
   Scenario: Game should update GUI when valid move is made by yellow player 2
-    Given that html div element with class .board exists when board render method is called when player 2 makes a move
-    And that board render method was called after move was made
-    When board currentPlayer property value was 2
+    Given that html div element with class .board exists when board render method is called when yellow player 2 makes a move
+    And that board render method was called after move was made by yellow player
+    When board currentPlayer property value was 2 on move by yellow player
     Then class .yellow should be added to one of the 42 html div elements corresponding to last player move
-    And all previous player moves should remain visible and correspond to values in property board matrix array
+    And all previous moves should remain visible including yellow player 2's and correspond to values in property board matrix array
 
   # Unfinished
   Scenario: Running the game when html div element with class .board is missing
--- step-definitions/Board-render-test.js
+++ step-definitions/Board-render-test.js
@@ -4,6 +4,17 @@ require('./_include-all')();
 
 module.exports = function () {
 
+  let renderCalled = false;
+  let fakeGame;
+
+  class FakeGame extends Game {
+    start() { this.board = new FakeBoard(this); }
+  }
+
+  class FakeBoard extends Board {
+    render() { renderCalled = true; }
+  }
+
 
   /* -------------------------------------------------------------------------------- */
   /* ---------- Scenario: Running the game should draw an empty game board ---------- */
@@ -11,15 +22,22 @@ module.exports = function () {
 
   this.Given(/^that board render method is called by board constructor when running the game$/, function () {
 
+    fakeGame = new FakeGame();
+
+    expect(renderCalled).to.be.true;
+
   });
+
 
   this.Given(/^that html div element with class \.board exists when board render method is called when running the game$/, function () {
 
-    expect($('.board')).to.be.true;
+    expect($('.board')).to.not.be.null;
 
   });
 
   this.Given(/^that this html div element with class \.board is empty$/, function () {
+
+    expect($('.board').innerHTML).to.equal('');
 
   });
 
@@ -32,15 +50,15 @@ module.exports = function () {
   /* ---------- Scenario: Game should update GUI when valid move is made by red player 1 ---------- */
   /* ---------------------------------------------------------------------------------------------- */
 
-  this.Given(/^that html div element with class \.board exists when board render method is called when player (\d+) makes a move$/, function (arg1) {
+  this.Given(/^that html div element with class \.board exists when board render method is called when red player (\d+) makes a move$/, function (arg1) {
 
   });
 
-  this.Given(/^that board render method was called after move was made$/, function () {
+  this.Given(/^that board render method was called after move was made by red player$/, function () {
 
   });
 
-  this.When(/^board currentPlayer property value was (\d+)$/, function (arg1) {
+  this.When(/^board currentPlayer property value was (\d+) on move by red player$/, function (arg1) {
 
   });
 
@@ -48,7 +66,7 @@ module.exports = function () {
 
   });
 
-  this.Then(/^all previous player moves should remain visible and correspond to values in property board matrix array$/, function () {
+  this.Then(/^all previous moves should remain visible including red player 1's and correspond to values in property board matrix array$/, function () {
 
   });
 
@@ -57,15 +75,15 @@ module.exports = function () {
   /* ---------- Scenario: Game should update GUI when valid move is made by yellow player 2 ---------- */
   /* ------------------------------------------------------------------------------------------------- */
 
-  this.Given(/^that html div element with class \.board exists when board render method is called when player (\d+) makes a move$/, function (arg1) {
+  this.Given(/^that html div element with class \.board exists when board render method is called when yellow player (\d+) makes a move$/, function (arg1) {
 
   });
 
-  this.Given(/^that board render method was called after move was made$/, function () {
+  this.Given(/^that board render method was called after move was made by yellow player$/, function () {
 
   });
 
-  this.When(/^board currentPlayer property value was (\d+)$/, function (arg1) {
+  this.When(/^board currentPlayer property value was (\d+) on move by yellow player$/, function (arg1) {
 
   });
 
@@ -73,7 +91,7 @@ module.exports = function () {
 
   });
 
-  this.Then(/^all previous player moves should remain visible and correspond to values in property board matrix array$/, function () {
+  this.Then(/^all previous moves should remain visible including yellow player 2's and correspond to values in property board matrix array$/, function () {
 
   });
 

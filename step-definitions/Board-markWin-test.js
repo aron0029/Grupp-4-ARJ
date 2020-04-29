5cadfd44f32e33c6c1ca6b642643272ba3bcb779
Paatsu <johnny@wong.se>
2020-04-29 18:34:53 +0200

    Board-markWin-test, småfix

--- step-definitions/Board-markWin-test.js
+++ step-definitions/Board-markWin-test.js
@@ -9,6 +9,7 @@ module.exports = function () {
   let markWinWasCalled = false;
   let tellTurnPlayer = [];
   let comboType;
+  let game;
 
   class FakeGame extends Game {
     start() { this.board = new FakeBoard(this); }
@@ -25,16 +26,13 @@ module.exports = function () {
   }
   class FakeBoard2 extends Board { }
 
-  let board;
-  let game;
-
   // Scenario: The game was won by a player
 
-  this.Given(/^a player wins the game$/, function () {
-    game = new FakeGame();
-    board = game.board;
+  this.Given(/^a player wins the game$/, async function () {
 
-    board.matrix = [
+    game = new FakeGame();
+
+    game.board.matrix = [
       [0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0],
@@ -42,56 +40,68 @@ module.exports = function () {
       [1, 0, 0, 0, 0, 0, 0],
       [1, 0, 0, 0, 0, 0, 0]
     ];
+    await game.board.makeMove(0);
+
   });
 
-  this.Given(/^board makeMove method called board markWin method$/, async function () {
-    await board.makeMove(0);
+  this.Given(/^board makeMove method called board markWin method$/, function () {
 
     expect(markWinWasCalled, 'markWin was not called').to.be.true;
+
   });
 
-  this.Then(/^the argument passed to markWin method should be a type array with a length of (\d+)$/, function (four) {
-    expect(comboType).to.be.an('array'
+  this.Then(/^the argument passed to markWin method should be a type "([^"]*)" with a length of (\d+)$/, function (typeArray, arrayLength) {
+
+    expect(comboType).to.be.an(typeArray
       , 'argument combo is not an array'
     );
-    expect(comboType, 'argument combo length should be 4').to.have.lengthOf(+four);
+
+    expect(comboType).to.have.lengthOf(+arrayLength
+      , 'argument combo length should be 4');
+
   });
 
-  this.Then(/^each of the (\d+) elements of this array should be a type 'array' with (\d+) elements each containing type 'number'$/, function (four, two) {
-    comboType.map(x => expect(x).to.be.an('array'
+  this.Then(/^each of the four elements of this array should be a type "([^"]*)" with (\d+) elements each containing type "([^"]*)"$/, function (typeArray, posValueLength, typeNumber) {
+
+    comboType.map(x => expect(x).to.be.an(typeArray
       , 'argument combo inner elements is not an array'
     ));
 
-    comboType.map(x => x.map(y => expect(y).to.be.a('number',
+    comboType.map(x => expect(x).to.have.lengthOf(+posValueLength
+      , 'each inner array of combo must be 2 in length'
+    ));
+
+    comboType.map(x => x.map(y => expect(y).to.be.a(typeNumber,
       'elements in combo is not containing numbers'
     )));
+
   });
 
-  this.Then(/^first of these (\d+) elements be set to a value between (\d+) to (\d+) and the second element be set to a value of (\d+) to (\d+)$/, function (two, zero, six, zero2, seven) {
-    comboType.map(x => expect(x[0]).to.be.within(zero, six,
-      'first element in combo is not between 0 and 6'
+  this.Then(/^first of these two elements be set to a value between (\d+) to (\d+) and the second element be set to a value of (\d+) to (\d+)$/, function (minRow, maxRow, minCol, maxCol) {
+
+    comboType.map(x => expect(x[0]).to.be.within(minRow, maxRow,
+      'first element in combo is not between 0 and 6 (row)'
     ))
-    comboType.map(x => expect(x[1]).to.be.within(zero2, seven,
-      'second element in combo is not between 0 and 7'
+    comboType.map(x => expect(x[1]).to.be.within(minCol, maxCol,
+      'second element in combo is not between 0 and 7 (col)'
     ))
 
   });
 
   this.Then(/^markWin should add the class \.win to html div elements that correspond to the winning rows position in board matrix$/, async function () {
-    let game2 = new FakeGame2();
-    board = game2.board;
-    board.render();
 
-    board.matrix = [
+    let game2 = new FakeGame2();
+
+    game2.board.matrix = [
       [0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0],
-      [1, 0, 0, 0, 0, 0, 0],
-      [1, 0, 0, 0, 0, 0, 0],
-      [1, 0, 0, 0, 0, 0, 0]
+      [1, 1, 0, 0, 0, 0, 0],
+      [1, 1, 0, 0, 0, 0, 0],
+      [1, 1, 0, 0, 0, 0, 0]
     ];
 
-    await board.makeMove(0);
+    await game2.board.makeMove(0);
     let winningIndex = [14, 21, 28, 35]
 
     for (let i = 0; i <= 3; i++) {
@@ -99,12 +109,14 @@ module.exports = function () {
         'markWin is not setting .win class in right div position'
       )
     }
+
   });
 
   // Scenario: The game was a draw
 
-  this.Given(/^the board is full without any player winning$/, function () {
-    board.matrix = [
+  this.Given(/^the board is full without any player winning$/, async function () {
+
+    game.board.matrix = [
       [2, 0, 2, 1, 2, 2, 1],
       [2, 1, 2, 1, 2, 2, 1],
       [2, 1, 2, 1, 2, 2, 1],
@@ -112,12 +124,15 @@ module.exports = function () {
       [1, 2, 1, 2, 1, 2, 2],
       [1, 2, 1, 2, 1, 2, 2]
     ];
+    markWinWasCalled = false;
+    await game.board.makeMove(1);
+
   });
 
-  this.Then(/^markWin should not be called$/, async function () {
-    markWinWasCalled = false;
-    await board.makeMove(1);
+  this.Then(/^markWin should not be called$/, function () {
+
     expect(markWinWasCalled, 'markWin should not be called when draw').to.be.false;
+
   });
 
 }

9773dbfb9a8178dbfd6ac76e75a142078dbc54dd
aron0029 <aronsellstedt@gmail.com>
2020-04-30 09:35:38 +0200

    Game-addEventListner-test

---
+++ step-definitions/Game-addEventListener-test.js
@@ -0,0 +1,33 @@
+require('./_include-all')();
+
+module.exports = function () {
+
+
+
+    
+
+
+this.Given(/^that board makeMove called board removeEventListener method$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+  
+
+  this.Then(/^board removeEventListener should remove eventlistener from div html element with class \.board$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+
+  this.Given(/^the game is over and makeMove called board removeEventListener method$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+
+  this.Then(/^method will use command  help method  \$ to get the right  element in the DOM$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+}
---
+++ step-definitions/Game-start-test.js
@@ -0,0 +1,39 @@
+require('./_include-all')();
+
+module.exports = function () {
+
+    
+
+
+this.When(/^method start\(\) is called$/, function (callback) {
+   expect 
+    callback(null, 'pending');
+  });
+
+
+
+  this.Then(/^it should create an instance of Board$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+
+
+  this.Then(/^send current instance of Game to Boards constructor$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+
+
+  this.Then(/^save the instance in property "([^"]*)"\.$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+}
+
+
+//start() {
+//    this.board = new Board(this);
+  //}

bc29b2697f33b3cb90809c5c4e00d57fa1913bd6
RikardLof <rikardlof@gmail.com>
2020-04-30 20:00:09 +0200

    Refactoring bort med mina throw som ej finns i API

--- www/Game.js
+++ www/Game.js
@@ -42,14 +42,10 @@ class Game {
   }
 
   addEventListener() {
-    if ($('.message')) {
-      $('.message').addEventListener("click", (event) => {
-        if (event.target.classList.contains('again')) { this.start(); }
-      });
-    }
-    else {
-      throw (new Error('Could not add .message eventlistener!'));
-    }
+    $('.message').addEventListener("click", (event) => {
+      if (event.target.classList.contains('again')) { this.start(); }
+    });
+
   }
 }
 

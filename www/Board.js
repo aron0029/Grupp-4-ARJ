70123679ff54c25e7621d1b829b383e344d5ae2e
aron0029 <aronsellstedt@gmail.com>
2020-05-02 12:03:42 +0200

    fix change

--- www/Board.js
+++ www/Board.js
@@ -1,12 +1,7 @@
 class Board {
 
-<<<<<<< HEAD
   constructor (game) {
-    if (!game instanceof Game) throw (new error('game must be an instance of Game'));
-=======
-  constructor(game) {
     if (!(game instanceof Game)) throw (new Error('game must be an instance of Game'));
->>>>>>> a89f799c4cd33d9b74993f51e7a0eac48cebcc98
     this.game = game;
 
     // Creating 6x7 2D array. All values set to 0
@@ -78,49 +73,6 @@ class Board {
   }
 
 
-<<<<<<< HEAD
-  // Change currentPlayer
-
-
-
-
-  winCheck() {
-
-    let winOffset = [
-      [[0, 0], [0, 1], [0, 2], [0, 3]],
-      [[0, 0], [1, 0], [2, 0], [3, 0]],
-      [[0, 0], [1, 1], [2, 2], [3, 3]],
-      [[0, 0], [1, 1], [2, -2], [3, -3]]
-    ];
-    for (let row = 0; row < 6; row++) {
-      for (let col = 0; col < 7; col++) {
-        for (let w of winOffset) {
-          let slots = w.map(([r, c]) => this.matrix[row + r] && this.matrix
-          [row + r][col + c]).join('');
-          if (slots === '1111' || '2222') {
-            return +slots[0];
-
-          }
-          else {
-
-            return false;
-          }
-        }
-
-
-      }
-    }
-
-
-
-
-  }
-
-
-  markWin(combo) {
-
-    //anropas med combo - egenskapen från winCheck som inargument.
-=======
   winCheck() {
     let winOffset = [
       [[0, 0], [0, 1], [0, 2], [0, 3]], // horizontal offset
@@ -172,7 +124,6 @@ class Board {
       let divMarkWin = ((win[0]) * 7) + (win[1]);
       boardDivs[divMarkWin].classList.add('win');
     }
->>>>>>> a89f799c4cd33d9b74993f51e7a0eac48cebcc98
   }
 
 
@@ -191,26 +142,13 @@ class Board {
       $('.board').addEventListener("click", this.listener);
     }
     else {
-<<<<<<< HEAD
-      throw new (error('Could not add .board eventlistener!'));
-=======
       throw (new Error('Could not add .board eventlistener!'));
->>>>>>> a89f799c4cd33d9b74993f51e7a0eac48cebcc98
     }
   }
 
 
   removeEventListener() {
-<<<<<<< HEAD
-    // whe  winCheck === true
-
-    /* Metoden ska ta bort händelselyssnaren lagrad i egenskapen listener från elementet med css - klassen board i DOM: en.
-
-   Metoden ska använda hjälpmetoden $ för att ta tag i rätt element i DOM: en.*/
-
-=======
     $('.board').removeEventListener('click', this.listener);
->>>>>>> a89f799c4cd33d9b74993f51e7a0eac48cebcc98
   }
 
 

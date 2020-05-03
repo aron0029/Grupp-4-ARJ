3ca67e9335e7cb05f516b49e3692f6bef6544743
aron0029 <aronsellstedt@gmail.com>
2020-05-03 21:05:30 +0200

    Game-start test update

--- step-definitions/Game-start-test.js
+++ step-definitions/Game-start-test.js
@@ -9,10 +9,6 @@ module.exports = function () {
   let Game;
 
 
-  // start()
-  // Metoden ska skapa ett ny instans av
-  // Board och skicka nuvarande instans av Game till dess konstruktor.
-  //Instansen ska lagras i egenskapen board
 
 
   this.Given(/^that the method start\(\) is called$/, function () {
@@ -20,11 +16,6 @@ module.exports = function () {
 
   });
 
-  /*2* Scenario: Starting a new game - features\Game - start.feature: 4
-  Step: Then it should create an instance of Board - features\Game - start.feature: 6
-  Message:
-  Undefined.Implement with the following snippet: */
-  function NewBoard() { }
 
 
   this.Then(/^it should create an instance of Board$/, function () {
@@ -32,21 +23,13 @@ module.exports = function () {
 
   });
 
-  /* 3) Scenario: Starting a new game - features\Game - start.feature: 4
-   Step: And send current instance of Game to Boards constructor - features\Game - start.feature: 7
-   Message:
-   Undefined.Implement with the following snippet:
-   */
+
 
   this.Then(/^send current instance of Game to Boards constructor$/, function () {
     expect(Game).to.be.an.instanceof.Board;
 
   });
 
-  /*4) Scenario: Starting a new game - features\Game - start.feature: 4
-  Step: And save the instance in property "board". - features\Game - start.feature: 8
-  Message:
-  Undefined.Implement with the following snippet: */
 
   this.Then(/^save the instance in property "([^"]*)"\.$/, function (value) {
     expect(Start).to.be.instanceof.Board;

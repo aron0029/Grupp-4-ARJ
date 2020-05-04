a67885544e02f6763896626de657a7516e7c411d
aron0029 <aronsellstedt@gmail.com>
2020-05-04 19:41:46 +0200

    Game-start-test done

--- step-definitions/Game-start-test.js
+++ step-definitions/Game-start-test.js
@@ -4,35 +4,49 @@ require('./_async-helpers.js');
 module.exports = function () {
   this.After(() => fixNoSuchWindowError(driver));
 
-  let Board;
-  let Start = false;
-  let Game;
 
+  let board;
+  let fakeGame;
+  let realGame;
+  let winCheck;
+  let startCalled = false;
 
+  class FakeGame extends Game {
+    start() { startCalled = true; }
+  }
+
+  fakeGame = new FakeGame();
+
+  realGame = new Game();
 
 
   this.Given(/^that the method start\(\) is called$/, function () {
-    expect(Start).to.be.callback;
+
+    expect(startCalled).to.be.true;
 
   });
 
 
 
+
   this.Then(/^it should create an instance of Board$/, function () {
-    expect(Board).to.be.instanceof.Start;
+
+    expect(realGame.board).to.be.instanceof(Board);
 
   });
 
 
 
   this.Then(/^send current instance of Game to Boards constructor$/, function () {
-    expect(Game).to.be.an.instanceof.Board;
+
+    expect(realGame).to.deep.equal(realGame.board.game);
 
   });
 
 
   this.Then(/^save the instance in property "([^"]*)"\.$/, function (value) {
-    expect(Start).to.be.instanceof.Board;
+
+    // Untestable?
 
   });
 
